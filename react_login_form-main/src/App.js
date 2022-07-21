48bbb274ffed83844c2d821d080013102b71ebfd
shubham <shubhamsavner@thoughtwin.com>
2022-07-21 16:58:32 +0530

    added new feature

---
+++ react_login_form-main/src/App.js
@@ -0,0 +1,14 @@
+import Register from './Register';
+import Login from './Login';
+
+function App() {
+
+  return (
+    <main className="App">
+      <Login />
+      <Register/>
+    </main>
+  );
+}
+
+export default App;
